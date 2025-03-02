d5e5dbd0a40b3de0afeca3223a9a2846534b0d4c
landseal <kenndesilva1@gmail.com>
2025-03-02 20:58:43 +1100

    ABSQuery get the XML string.

--- readabs/abs_query.py
+++ readabs/abs_query.py
@@ -1,20 +1,49 @@
 from __future__ import annotations
 from typing import NewType
 
+import re
+
+import readabs.connection as conn
+
 # Types
 CatNo = NewType('CatNo', str)
 SeriesID = NewType('SeriesID', str)
-type ABSID = CatNo | SeriesID
+ABSXML = NewType('ABSXML', str)
+
+# Exception Class
+class ABSQueryError(Exception):
+    pass
 
 # Class
 class ABSQuery:
+    _base_query: str = r"https://abs.gov.au:443/servlet/TSSearchServlet\?"
+
     def __init__(self: ABSQuery, catNo: str | None = None, seriesID: str | None = None):
+
+        self.catNo: CatNo | None = None
+        self.seriesID: SeriesID | None = None
        
+        # These should be mutually exclusive.
         if catNo is not None:
+            if re.search(r"\.0$", catNo) is None:
+                raise ABSQueryError("catNo must end in '.0'")
+
             self.catNo = CatNo(catNo)
+            self.seriesID = None
 
         else:
             if seriesID is not None:
+                self.catNo = None
                 self.seriesID = SeriesID(seriesID)
             else:
-                raise ValueError("Either catNo or seriesID must be provided")
+                raise ABSQueryError("Either catNo or seriesID must be provided")
+
+    def _construct_ts_dict_query(self: ABSQuery) -> str:
+        id_str: str = f"catno={self.catNo}" if self.catNo is not None else f"sid={self.seriesID}"
+
+        return self._base_query + f"{id_str}" 
+
+    def _get_ts_dict_xml(self: ABSQuery) -> ABSXML:
+        xml_query: str = self._construct_ts_dict_query()
+
+        return ABSXML(conn._get_data(xml_query).text)
--- tests/test_abs_query.py
+++ tests/test_abs_query.py
@@ -1,3 +1,5 @@
+import pytest
+
 import readabs.abs_query as module
 
 def test_abs_query_exists():
@@ -5,3 +7,27 @@ def test_abs_query_exists():
     abs_query: module.ABSQuery = module.ABSQuery("5340.0")
 
     assert isinstance(abs_query, module.ABSQuery)
+
+def test_inappropriate_catNo():
+    with pytest.raises(module.ABSQueryError):
+        abs_query: module.ABSQuery = module.ABSQuery("4349")
+
+def test_both_none():
+    with pytest.raises(module.ABSQueryError):
+        abs_query: module.ABSQuery = module.ABSQuery()
+
+def test_xml_query():
+    abs_query: module.ABSQuery = module.ABSQuery("5340.0")
+
+    assert isinstance(abs_query._construct_ts_dict_query(), str)
+
+def test_xml_string():
+    abs_query: module.ABSQuery = module.ABSQuery("5340.0")
+
+    assert isinstance(abs_query._get_ts_dict_xml(), str)
+
+def test_xml_return():
+    abs_query: module.ABSQuery = module.ABSQuery("5340.0")
+    error_str: str = '<?xml version="1.0" encoding="utf-8" ?><Error>Invalid query.</Error>\r\n'
+
+    assert abs_query._get_ts_dict_xml() != error_str 
