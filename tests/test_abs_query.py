3f2c6cd59e1ae1664416e1a54929ffe4b041c327
landseal <kenndesilva1@gmail.com>
2025-03-09 16:52:32 +1100

    Add mock plugin. Speed up tests.

--- tests/test_abs_query.py
+++ tests/test_abs_query.py
@@ -1,14 +1,26 @@
+from pathlib import Path
+
 import pandas as pd
 import xml.etree.ElementTree as ET
 import requests as req
 import pytest_mock as mock
 
 import pytest
+import xmltodict
+
 import readabs.abs_query as module
 
-ABS_URL = "https://ausstats.abs.gov.au/servlet/TSSearchServlet?"
-EXAMPLE_CAT_NO = "6401.0"
-EXAMPLE_TABLE = "TABLES 1 and 2."
+ABS_URL: str = "https://ausstats.abs.gov.au/servlet/TSSearchServlet?"
+EXAMPLE_CAT_NO: str = "6401.0"
+EXAMPLE_TABLE: str = "TABLES 1 and 2."
+RES_PATH: Path = Path(__file__).parent / 'res'
+
+@pytest.fixture
+def xml_magic_mock():
+    with open(RES_PATH / 'sample_timeseries_dict.xml', 'r') as f:
+        xml_file: str = f.read() 
+
+    return xml_file
 
 def test_get_data():
     response: req.models.Response = req.get(ABS_URL)
@@ -33,29 +45,41 @@ def test_abs_query_exists():
 
 def test_inappropriate_catNo():
     with pytest.raises(module.ABSQueryError):
-        abs_query: module.ABSQuery = module.ABSQuery("4349")
+        module.ABSQuery("4349")
 
 def test_both_none():
     with pytest.raises(module.ABSQueryError):
-        abs_query: module.ABSQuery = module.ABSQuery()
+        module.ABSQuery()
 
 def test_construct_query():
     abs_query: module.ABSQuery = module.ABSQuery(EXAMPLE_CAT_NO)
 
     assert isinstance(abs_query._construct_query(), str)
 
-def test_timeseries_dict_xml(mocker: mock.MockerFixture):
+def test_timeseries_dict_xml(mocker: mock.MockerFixture, xml_magic_mock):
+    mock_response = mocker.MagicMock()
+    mock_response.text = xml_magic_mock
+    mocker.patch("requests.get", return_value = mock_response)
+
     abs_query: module.ABSQuery = module.ABSQuery(EXAMPLE_CAT_NO)
 
     assert isinstance(abs_query._get_timeseries_dict_xml(), ET.Element)
 
-def test_xml_return():
+def test_xml_return(mocker: mock.MockerFixture, xml_magic_mock):
+    mock_response = mocker.MagicMock()
+    mock_response.text = xml_magic_mock
+    mocker.patch("requests.get", return_value = mock_response)
+
     abs_query: module.ABSQuery = module.ABSQuery(EXAMPLE_CAT_NO)
     error_str: str = '<?xml version="1.0" encoding="utf-8" ?><Error>Invalid query.</Error>\r\n'
 
     assert abs_query._get_timeseries_dict_xml().__str__ != error_str 
 
-def test_get_serieslist():
+def test_get_serieslist(mocker: mock.MockerFixture, xml_magic_mock):
+    mock_response = mocker.MagicMock()
+    mock_response.text = xml_magic_mock
+    mocker.patch("requests.get", return_value = mock_response)
+
     abs_query: module.ABSQuery = module.ABSQuery(EXAMPLE_CAT_NO)
     abs_query._get_serieslist()
 
@@ -63,13 +87,21 @@ def test_get_serieslist():
     
     assert series_list != []
 
-def test_get_table_names():
+def test_get_table_names(mocker: mock.MockerFixture, xml_magic_mock):
+    mock_response = mocker.MagicMock()
+    mock_response.text = xml_magic_mock
+    mocker.patch("requests.get", return_value = mock_response)
+
     abs_query: module.ABSQuery = module.ABSQuery(EXAMPLE_CAT_NO)
     
     # Empty check for sets
     assert abs_query.get_table_names()
 
-def test_get_table_link():
+def test_get_table_link(mocker: mock.MockerFixture, xml_magic_mock):
+    mock_response = mocker.MagicMock()
+    mock_response.text = xml_magic_mock
+    mocker.patch("requests.get", return_value = mock_response)
+
     abs_query: module.ABSQuery = module.ABSQuery(EXAMPLE_CAT_NO)
     table_links: dict[str, str] | None = abs_query.get_table_link(EXAMPLE_TABLE)
 
@@ -77,8 +109,12 @@ def test_get_table_link():
     assert table_links # Zero length test
 
 def test_get_dataframe():
+    table_link: dict[str, str] = {
+        'TABLES 1 and 2. CPI: All Groups, Index Numbers and Percentage Changes'
+        : 'https://www.abs.gov.au/statistics/economy/price-indexes-and-inflation/consumer-price-index-australia/latest-release/640101.xlsx' 
+    }
+
     abs_query: module.ABSQuery = module.ABSQuery(EXAMPLE_CAT_NO)
-    table_link: dict[str, str] | None = abs_query.get_table_link(EXAMPLE_TABLE)
 
     if table_link:
         for _, value in table_link.items():
